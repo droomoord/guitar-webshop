a5667c5734cf86034411ee2e096c8288ef56ac1b
Kris <heijnenkris@gmail.com>
2020-09-18 19:51:42 +0200

    add styling

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                 <div class="text">
                     <h1>Don't get out of your chair...</h1>
                     <p>...and start playing a new guitar! Search through our webshop and order yourself a six-string today!</p>
-                    <button onclick="window.location.href = 'browse.php'" value="browse.php">Let's go!</button>
+                    <button onclick="window.location.href = 'browse.php'">Let's go!</button>
                 </div>
             </div>
             <svg viewBox="0 0 1440 320"><path fill="white" fill-opacity="1" d="M0,64L48,96C96,128,192,192,288,192C384,192,480,128,576,133.3C672,139,768,213,864,245.3C960,277,1056,267,1152,234.7C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
